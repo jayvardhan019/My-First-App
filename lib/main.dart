42284d3f12b9d9a1382290f7b7c548e8ca08857c
Jay Vardhan <jayvardhan015@gmail.com>
2018-08-16 22:41:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "MyApp",
+      home: new HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.light,
+        accentColor: Colors.red,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String mytext = "Hello World";
+  void _changeText(){
+  setState((){
+    if(mytext.startsWith("H")){
+      mytext = "welcome to my app";
+    }else{
+      mytext = "Hello World";
+    }
+  });
+}
+
+  Widget _bodyWidget(){
+    return new Container(
+    padding: const EdgeInsets.all(8.0),
+     child: new Center(
+       child: new Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[
+          new Text(mytext),
+         ],
+       ),
+     ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text("HomePage"),
+      ),
+      body: _bodyWidget(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        onPressed: _changeText,
+      ),
+    );
+  }
+}
+
+
